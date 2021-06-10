36c7891a376868c636827b534199dd037d46d3e1
Pushpit <pushpit07@gmail.com>
2021-06-10 19:17:52 +0530

    Added neumorphism and working resultant scales section
    Signed-off-by: Pushpit <pushpit07@gmail.com>

--- static/home.js
+++ static/home.js
@@ -1,3 +1,61 @@
+const recordButton = document.querySelector('.record_button');
+const playButton = document.querySelector('.play_button');
+
+recordButton.addEventListener('click', toggleRecording);
+playButton.addEventListener('click', playSong);
+
+function toggleRecording() {
+    console.log('toggle')
+    recordButton.classList.toggle('active');
+    recordButton.innerHTML = `<i class="fas fa-microphone-alt"></i>&nbsp;&nbsp;Recording...`
+
+    if (isRecording()) {
+        console.log('start')
+        startRecording();
+    } else {
+        console.log('stop')
+        stopRecording();
+    }
+}
+
+function isRecording() {
+    return recordButton != null && recordButton.classList.contains('active');
+}
+
+let recordingStartTime;
+let songNotes;
+function startRecording() {
+    recordingStartTime = Date.now();
+    songNotes = [];
+    playButton.classList.remove('show');
+}
+
+function stopRecording() {
+    playSong();
+    playButton.classList.add('show');
+    recordButton.innerHTML = `<i class="fas fa-microphone"></i>&nbsp;&nbsp;Record`
+}
+
+function playSong() {
+    if (songNotes.length === 0)
+        return;
+    songNotes.forEach(note => {
+        setTimeout(() => {
+            currNote = document.querySelector(`.piano_key[data-note="${note.key}"]`)
+            playNote(currNote)
+        }, note.startTime)
+    })
+    console.log(songNotes);
+}
+
+function recordNote(note) {
+    songNotes.push({
+        key: note,
+        startTime: Date.now() - recordingStartTime
+    })
+}
+
+
 function getSelectedNotes() {
     var notes = [];
     var markedCheckbox = document.querySelectorAll('input[type="checkbox"]:checked');
@@ -57,18 +115,47 @@ function sendNotes(notes) {
             // }
             // $("#inputted_notes").append(inputtedNotes);
 
+            // $("#possible_chords").html('');
+            // var possChords = '';
+            // for (var scale in response.possible_chords_in_possible_scales) {
+            //     possChords += '<div class="row result_row"><div class="col-1 text-center"><span class="note possible_scale_name">Scale ' + scale + '</span></div>';
+            //     for (var note in response.possible_chords_in_possible_scales[scale]) {
+            //         possChords +=
+            //             '<div class="col text-center justify-content-center"><div class="row"><span class="note mb-4">' + note + '</span></div>' + '<div class="btn-group-vertical btn_group_width" role="group" aria-label="Basic radio toggle button group">'
+            //             + makeRadioButtons(response.possible_chords_in_possible_scales[scale][note], scale, note)
+            //             + '</div></div>';
+            //     }
+            //     possChords += '</div>'
+            // }
+
             $("#possible_chords").html('');
             var possChords = '';
+            possChords += '<div class="d-flex align-items-start mb-5"><div class="nav flex-column nav-pills me-3" id="v-pills-tab" role="tablist" aria-orientation="vertical">'
+
             for (var scale in response.possible_chords_in_possible_scales) {
-                possChords += '<div class="row result_row"><div class="col-1 text-center"><span class="note possible_scale_name">Scale ' + scale + '</span></div>';
+                if (scale === Object.keys(response.possible_chords_in_possible_scales)[0])
+                    possChords += '<button class="nav-link active result_scale_pill mt-4" id="v-pills-profile-tab" data-bs-toggle="pill" data-bs-target="#' + scale + 'abcd' + '" type="button" role="tab" aria-controls="v-pills-profile" aria-selected="false">Scale ' + scale + '</button>';
+                else
+                    possChords += '<button class="nav-link result_scale_pill mt-4" id="v-pills-profile-tab" data-bs-toggle="pill" data-bs-target="#' + scale + 'abcd' + '" type="button" role="tab" aria-controls="v-pills-profile" aria-selected="false">Scale ' + scale + '</button>';
+            }
+            possChords += '</div><div class="card flex-fill flex-row shadow-inset border-light p-5 ml-4 rounded"><div class="card-body flex-fill flex-row p-0"><div class="tab-content flex-row" id="v-pills-tabContent">';
+
+            for (var scale in response.possible_chords_in_possible_scales) {
+                if (scale === Object.keys(response.possible_chords_in_possible_scales)[0])
+                    possChords += '<div class="tab-pane show active flex-fill fade" id=' + scale + 'abcd' + ' role="tabpanel" aria-labelledby="v-pills-profile-tab"><div class="row">'
+                else
+                    possChords += '<div class="tab-pane flex-fill fade" id=' + scale + 'abcd' + ' role="tabpanel" aria-labelledby="v-pills-profile-tab"><div class="row">'
+
                 for (var note in response.possible_chords_in_possible_scales[scale]) {
                     possChords +=
-                        '<div class="col text-center"><div class="row"><span class="note mb-4">' + note + '</span></div>' + '<div class="btn-group-vertical btn_group_width" role="group" aria-label="Basic radio toggle button group">'
+                        '<div class="col text-center justify-content-center resultant_no_shadow"><div class="row"><span class="note mb-4">' + note + '</span></div>' + '<div class="btn-group-vertical btn_group_width resultant_no_shadow" role="group" aria-label="Basic radio toggle button group">'
                         + makeRadioButtons(response.possible_chords_in_possible_scales[scale][note], scale, note)
                         + '</div></div>';
                 }
-                possChords += '</div>'
+                possChords += '</div></div>'
             }
+            possChords += '</div></div></div></div>'
+
             // possChords += '<span class="selected_note"> with ' + response.max_matched_count + ' notes matching</span>';
             $("#possible_chords").append(possChords);
         },
@@ -83,7 +170,7 @@ function makeRadioButtons(array, scale, note) {
     radio_btn_group_name = scale + note;
     for (var i = 0; i < array.length; i++) {
         unique_name = scale + note + array[i];
-        btn_array += '<input type="radio" class="btn-check" name=' + note + ' id=' + unique_name + ' value=' + array[i] + ' autocomplete="off" /><label class="btn btn-outline-primary btn_resultant_chord_in_scale" for=' + unique_name + ' >' + array[i] + '</label>';
+        btn_array += '<input type="radio" class="btn-check resultant_no_shadow" name=' + note + ' id=' + unique_name + ' value=' + array[i] + ' autocomplete="off" /><label class="btn btn-outline-primary btn_resultant_chord_in_scale resultant_no_shadow" for=' + unique_name + ' >' + array[i] + '</label>';
     }
     // btn_array += '<div class="btn-group" role="group" aria-label="Basic radio toggle button group">';
 
@@ -115,7 +202,6 @@ function disp(result) {
     const spaceBtn = document.querySelector('#space');
     const backspaceBtn = document.querySelector('#Backspace');
     const display = document.querySelector('.display');
-
 
     if (result !== "") {
         text_earlier = form.textarea.value.toString();
@@ -192,10 +278,70 @@ function disp(result) {
 }
 
 
+function playNote(event) {
+    if (event.keyCode !== undefined) {
+        const noteAudio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+        const key = document.querySelector(`.piano_key[data-key="${event.keyCode}"]`);
+
+        if (!key)
+            return;
+
+        noteAudio.currentTime = 0;
+        noteAudio.play();
+
+        if (document.getElementById(event.key).classList.contains("sharp_note_key")) {
+            document.getElementById(event.key).classList.add("sharp_note_key_pressed");
+        } else {
+            document.getElementById(event.key).classList.add("keyPressed");
+        }
+
+        noteAudio.addEventListener('ended', () => {
+            if (document.getElementById(event.key).classList.contains("sharp_note_key")) {
+                document.getElementById(event.key).click();
+                document.getElementById(event.key).classList.remove("sharp_note_key_pressed");
+            }
+            else {
+                document.getElementById(event.key).click();
+                document.getElementById(event.key).classList.remove("keyPressed");
+            }
+        })
+    } else {
+        console.log(event)
+        const noteAudio = document.querySelector(`audio[data-key="${event.getAttribute("data-key")}"]`);
+        const key = document.querySelector(`.piano_key[data-key="${event.getAttribute("data-key")}"]`);
+
+        if (!key)
+            return;
+
+        noteAudio.currentTime = 0;
+        noteAudio.play();
+
+        if (document.getElementById(event.getAttribute("id")).classList.contains("sharp_note_key")) {
+            document.getElementById(event.getAttribute("id")).classList.add("sharp_note_key_pressed");
+        } else {
+            document.getElementById(event.getAttribute("id")).classList.add("keyPressed");
+        }
+
+        noteAudio.addEventListener('ended', () => {
+            if (document.getElementById(event.getAttribute("id")).classList.contains("sharp_note_key")) {
+                document.getElementById(event.getAttribute("id")).click();
+                document.getElementById(event.getAttribute("id")).classList.remove("sharp_note_key_pressed");
+            }
+            else {
+                document.getElementById(event.getAttribute("id")).click();
+                document.getElementById(event.getAttribute("id")).classList.remove("keyPressed");
+            }
+        })
+    }
+}
+
 window.addEventListener('keydown', function (event) {
     event.preventDefault();
     if (event.repeat)
         return;
+
+    if (isRecording())
+        recordNote(event.key.toUpperCase());
 
     if (event.keyCode === 32) {
         document.getElementById('space').classList.add("keyPressed");
@@ -204,21 +350,9 @@ window.addEventListener('keydown', function (event) {
         document.getElementById('back_arrow').classList.add("backspace_arrow_pressed");
     } else if (document.getElementById(event.key).classList.contains("disabled_key")) {
         // don't do anything
-    } else if (document.getElementById(event.key).classList.contains("sharp_note_key")) {
-        document.getElementById(event.key).classList.add("sharp_note_key_pressed");
+    } else {
+        playNote(event);
     }
-    else {
-        document.getElementById(event.key).classList.add("keyPressed");
-    }
-
-    const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
-    const key = document.querySelector(`.piano_key[data-key="${event.keyCode}"]`);
-
-    if (!key)
-        return;
-
-    audio.currentTime = 0;
-    audio.play();
 }, false);
 
 window.addEventListener('keyup', function (event) {
@@ -233,12 +367,12 @@ window.addEventListener('keyup', function (event) {
         document.getElementById('back_arrow').classList.remove("backspace_arrow_pressed");
     } else if (document.getElementById(event.key).classList.contains("disabled_key")) {
         // don't do anything
-    } else if (document.getElementById(event.key).classList.contains("sharp_note_key")) {
-        document.getElementById(event.key).click();
-        document.getElementById(event.key).classList.remove("sharp_note_key_pressed");
-    }
-    else {
-        document.getElementById(event.key).click();
-        document.getElementById(event.key).classList.remove("keyPressed");
+    } else {
+        if (document.getElementById(event.key).classList.contains("sharp_note_key")) {
+            document.getElementById(event.key).classList.remove("sharp_note_key_pressed");
+        }
+        else {
+            document.getElementById(event.key).classList.remove("keyPressed");
+        }
     }
 }, false);
